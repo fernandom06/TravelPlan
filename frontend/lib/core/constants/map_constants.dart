db71ce06cfc3ad0344b5440b2c71451cb796feaa
Fernando Muñoz Paredes <fernandom06@hotmail.com>
2026-08-22 18:34:57 +0200

    feat(map): add map constants for Neila center and OSM tiles

---
+++ frontend/lib/core/constants/map_constants.dart
@@ -0,0 +1,11 @@
+import 'package:latlong2/latlong.dart';
+
+const kDefaultCenter = LatLng(42.0414, -3.0428);
+
+const kDefaultZoom = 13.0;
+
+const kMinZoom = 3.0;
+
+const kMaxZoom = 18.0;
+
+const kOsmTileUrlTemplate = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
